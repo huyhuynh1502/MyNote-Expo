8115b72a0091ec2f74d1ec80c16c5280b707889d
huyhuynh1502 <huy.huynh@dal.ca>
2024-05-14 18:11:43 -0300

    [WIP] 2 column layout is created -> Masonry List still need to be done

--- components/NoteBlock.jsx
+++ components/NoteBlock.jsx
@@ -7,9 +7,9 @@ const content = "Test Content";
 
 const NoteBlock = ({title, content}) => {
     return (
-        <TouchableOpacity style={tw`bg-blue-300 p-5 m-2`}>
+        <TouchableOpacity style={tw`flex-1 bg-blue-300 p-5 m-3 w-50`}>
             <Text style={tw`text-2xl font-bold`}>{title}</Text>
-            <Text style={tw`text-1xl`}>{content}</Text>
+            <Text style={tw`text-xl`}>{content}</Text>
         </TouchableOpacity>
     )
 }
--- components/NoteBlockDisplay.jsx
+++ components/NoteBlockDisplay.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import tw from 'twrnc';
-import { SafeAreaView, View, ScrollView } from 'react-native';
+import { SafeAreaView, View, ScrollView, FlatList } from 'react-native';
 import NoteBlock from './NoteBlock';
 
 let nbs = [];
+
+nbs.push({ title: "Block 10", content: "Loremsiopsadfafdsfafsffsdff sa df asdfasf" });
 for (var i = 0; i < 10; i++) {
     nbs.push({ title: `Block ${i}`, content: `Content ${i}` });
 }
 
+
+
 const NoteBlockDisplay = () => {
   return (
     <>
-        <SafeAreaView style={tw`flex`}>
-            <ScrollView>
-                {nbs.map((nb, index) => (
-                    <NoteBlock 
-                    key={index} title={nb.title} content={nb.content} />
-                ))}
-            </ScrollView>
-            
-        </SafeAreaView>
+        <SafeAreaView style={tw`flex-1`}>
+            <FlatList
+                data={nbs}
+                renderItem={({ item }) => (
+                    <View style={tw``}>
+                        <NoteBlock 
+                        title={item.title} 
+                        content={item.content} 
+                    />
+                    </View>
+                    
+                )}
+                keyExtractor={(item, index) => index.toString()}
+                numColumns={2}
+            />
+    </SafeAreaView>
     </>
   )
 }
